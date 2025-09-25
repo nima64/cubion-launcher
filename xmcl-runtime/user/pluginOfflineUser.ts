188751ca679a868ec311107e5784cd9ea994d592
nima64 <kennethe407@gmail.com>
2025-09-25 03:43:33 -0700

    Remove the ability to create or manage offline accounts

--- xmcl-runtime/user/pluginOfflineUser.ts
+++ xmcl-runtime/user/pluginOfflineUser.ts
@@ -30,77 +30,77 @@ export const pluginOffineUser: LauncherAppPlugin = async (app) => {
       }
     }
   })
-  userService.registerAccountSystem(AUTHORITY_DEV, {
-    async login({ username, properties }) {
-      const auth = offline(username, properties?.uuid)
-      auth.selectedProfile.id = getUUID(username)
+  // userService.registerAccountSystem(AUTHORITY_DEV, {
+  //   async login({ username, properties }) {
+  //     const auth = offline(username, properties?.uuid)
+  //     auth.selectedProfile.id = getUUID(username)
 
-      const existed = userService.state.users[OFFLINE_USER_ID]
-      const profiles = existed ? { ...existed.profiles } : {}
-      for (const p of auth.availableProfiles) {
-        profiles[p.id] = {
-          name: p.name,
-          id: p.id,
-          uploadable: ['cape', 'skin'],
-          textures: {
-            SKIN: {
-              url: '',
-              metadata: {},
-            },
-          },
-        }
-      }
+  //     const existed = userService.state.users[OFFLINE_USER_ID]
+  //     const profiles = existed ? { ...existed.profiles } : {}
+  //     for (const p of auth.availableProfiles) {
+  //       profiles[p.id] = {
+  //         name: p.name,
+  //         id: p.id,
+  //         uploadable: ['cape', 'skin'],
+  //         textures: {
+  //           SKIN: {
+  //             url: '',
+  //             metadata: {},
+  //           },
+  //         },
+  //       }
+  //     }
 
-      const profile: UserProfile = {
-        id: OFFLINE_USER_ID,
-        invalidated: false,
-        selectedProfile: auth.selectedProfile.id,
-        profiles,
-        expiredAt: Number.MAX_SAFE_INTEGER / 100 * 95,
-        authority: AUTHORITY_DEV,
-        username: OFFLINE_USER_ID,
-      }
-      await userTokenStorage.put(profile, auth.accessToken)
+  //     const profile: UserProfile = {
+  //       id: OFFLINE_USER_ID,
+  //       invalidated: false,
+  //       selectedProfile: auth.selectedProfile.id,
+  //       profiles,
+  //       expiredAt: Number.MAX_SAFE_INTEGER / 100 * 95,
+  //       authority: AUTHORITY_DEV,
+  //       username: OFFLINE_USER_ID,
+  //     }
+  //     await userTokenStorage.put(profile, auth.accessToken)
 
-      return profile
-    },
-    async setSkin(p, gameProfile, { skin, cape }) {
-      if (skin !== undefined) {
-        if (skin) {
-          let url = skin.url
-          if (!url.startsWith('http')) {
-            url = await imageStore.addImage(url)
-          }
-          gameProfile.textures.SKIN.url = url
-          gameProfile.textures.SKIN.metadata = { model: skin.slim ? 'slim' : 'steve' }
-        } else {
-          gameProfile.textures.SKIN.url = ''
-          gameProfile.textures.SKIN.metadata = undefined
-        }
-      }
-      if (cape !== undefined) {
-        if (cape) {
-          let url = cape
-          if (!url.startsWith('http')) {
-            url = await imageStore.addImage(url)
-          }
-          gameProfile.textures.CAPE = { url }
-        } else {
-          gameProfile.textures.CAPE = undefined
-        }
-      }
-      return p
-    },
-    async refresh(p) { return p },
-    getSupporetedAuthorityMetadata: function (allowThirdparty: boolean): AuthorityMetadata[] {
-      if (!allowThirdparty) return []
-      return [
-        {
-          authority: AUTHORITY_DEV,
-          flow: ['anonymous'],
-          kind: 'builtin',
-        }
-      ]
-    }
-  })
+  //     return profile
+  //   },
+  //   async setSkin(p, gameProfile, { skin, cape }) {
+  //     if (skin !== undefined) {
+  //       if (skin) {
+  //         let url = skin.url
+  //         if (!url.startsWith('http')) {
+  //           url = await imageStore.addImage(url)
+  //         }
+  //         gameProfile.textures.SKIN.url = url
+  //         gameProfile.textures.SKIN.metadata = { model: skin.slim ? 'slim' : 'steve' }
+  //       } else {
+  //         gameProfile.textures.SKIN.url = ''
+  //         gameProfile.textures.SKIN.metadata = undefined
+  //       }
+  //     }
+  //     if (cape !== undefined) {
+  //       if (cape) {
+  //         let url = cape
+  //         if (!url.startsWith('http')) {
+  //           url = await imageStore.addImage(url)
+  //         }
+  //         gameProfile.textures.CAPE = { url }
+  //       } else {
+  //         gameProfile.textures.CAPE = undefined
+  //       }
+  //     }
+  //     return p
+  //   },
+  //   async refresh(p) { return p },
+  //   getSupporetedAuthorityMetadata: function (allowThirdparty: boolean): AuthorityMetadata[] {
+  //     if (!allowThirdparty) return []
+  //     return [
+  //       {
+  //         authority: AUTHORITY_DEV,
+  //         flow: ['anonymous'],
+  //         kind: 'builtin',
+  //       }
+  //     ]
+  //   }
+  // })
 }
